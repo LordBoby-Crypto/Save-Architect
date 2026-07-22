670d169ed5cfc29b91816c52aadfcc99eaf46f7d
LordBoby-Crypto <zacmarlin4@gmail.com>
2026-07-22 09:50:35 -0500

    Add Save Architect package

---
+++ src/savearchitect/__init__.py
@@ -0,0 +1,6 @@
+"""Save Architect core package."""
+
+from .analysis import AnalysisReport, FileAnalyzer, FileFingerprint
+
+__all__ = ["AnalysisReport", "FileAnalyzer", "FileFingerprint"]
+__version__ = "0.1.0"
